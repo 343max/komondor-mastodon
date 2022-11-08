1b19719588901253563a08e1b607d70b4f80b16f
Max von Webel <max@343max.de>
2022-11-08 17:39:51 +0100

    better repost

--- src/components/StatusView.tsx
+++ src/components/StatusView.tsx
@@ -1,4 +1,4 @@
-import { SimpleLineIcons } from "@expo/vector-icons"
+import { EvilIcons, SimpleLineIcons } from "@expo/vector-icons"
 import { Status } from "masto"
 import React from "react"
 import { View, StyleProp, ViewStyle, TouchableOpacity } from "react-native"
@@ -15,37 +15,54 @@ type Props = {
 export const StatusView: React.FC<Props> = ({ status, style }) => {
   const { colors } = useTheme()
   const [innerWidth, setInnerWidth] = React.useState(0)
+  const isRepost = status.reblog != null
+  const displayStatus = status.reblog ?? status
   return (
-    <View style={[tw`flex-row w-full`, style]}>
-      <Avatar uri={status.account.avatarStatic} size={30} style={tw`mr-3`} />
-      <View
-        style={tw`grow`}
-        onLayout={({ nativeEvent }) => setInnerWidth(nativeEvent.layout.width)}
-      >
-        <Text variant="titleSmall">
-          {status.account.displayName}
-          <TouchableOpacity
-            onPress={() => {
-              console.log(JSON.stringify(status, null, 2))
-            }}
-          >
-            <SimpleLineIcons
-              name="wrench"
-              size={12}
-              color={colors.primary}
-              style={tw`pl-2`}
-            />
-          </TouchableOpacity>
-        </Text>
-        <Text variant="titleSmall">{status.account.acct}</Text>
-        {status.reblog ? (
-          <StatusView status={status.reblog} style={tw`mt-2`} />
-        ) : (
-          <Text style={{ width: innerWidth }}>{status.content}</Text>
-        )}
-        {status.mediaAttachments.map((attachment) => (
-          <MediaAttachmentView attachment={attachment} />
-        ))}
+    <View style={style}>
+      {isRepost ? (
+        <View style={tw`mb-1 flex-row`}>
+          <EvilIcons name="retweet" size={24} color={colors.onSurface} />
+          <Avatar
+            uri={status.account.avatarStatic}
+            size={18}
+            style={tw`mx-1`}
+          />
+          <Text variant="titleSmall">{status.account.displayName}</Text>
+        </View>
+      ) : null}
+      <View style={tw`flex-row w-full`}>
+        <Avatar
+          uri={displayStatus.account.avatarStatic}
+          size={30}
+          style={tw`mr-3`}
+        />
+        <View
+          style={tw`grow`}
+          onLayout={({ nativeEvent }) =>
+            setInnerWidth(nativeEvent.layout.width)
+          }
+        >
+          <Text variant="titleSmall">
+            {displayStatus.account.displayName}
+            <TouchableOpacity
+              onPress={() => {
+                console.log(JSON.stringify(status, null, 2))
+              }}
+            >
+              <SimpleLineIcons
+                name="wrench"
+                size={12}
+                color={colors.primary}
+                style={tw`pl-2`}
+              />
+            </TouchableOpacity>
+          </Text>
+          <Text variant="titleSmall">{displayStatus.account.acct}</Text>
+          <Text style={{ width: innerWidth }}>{displayStatus.content}</Text>
+          {displayStatus.mediaAttachments.map((attachment) => (
+            <MediaAttachmentView attachment={attachment} />
+          ))}
+        </View>
       </View>
     </View>
   )
