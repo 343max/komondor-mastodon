a5699a5a96ca3b170d9c7cb43d1540fca46fa233
Max von Webel <max@343max.de>
2022-11-09 10:39:25 +0100

    no more key in list warning

--- src/components/HtmlText.tsx
+++ src/components/HtmlText.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { Text } from "react-native-paper"
 import { parse, Node, HTMLElement } from "node-html-parser"
 import { StyleProp, TextStyle, View } from "react-native"
@@ -18,20 +19,21 @@ export const HtmlText: React.FC<Props> = ({ text, style }) => {
     index: number,
     textStyle: StyleProp<TextStyle> | undefined
   ): React.ReactElement<any, any> | null => {
+    const renderChildNodes = (ts: StyleProp<TextStyle> | undefined) =>
+      node.childNodes.map((n, i) => (
+        <React.Fragment key={i}>{renderViews(n, i, ts)}</React.Fragment>
+      ))
+
     if (!(node instanceof HTMLElement)) {
       return <Text style={textStyle}>{node.textContent}</Text>
     } else {
       if (node.tagName === null) {
-        return (
-          <Text style={style}>
-            {node.childNodes.map((n, i) => renderViews(n, i, textStyle))}
-          </Text>
-        )
+        return <Text style={style}>{renderChildNodes(textStyle)}</Text>
       } else if (node.tagName === "P") {
         return (
           <Text>
             {index > 0 ? "\n\n" : ""}
-            {node.childNodes.map((n, i) => renderViews(n, i, textStyle))}
+            {renderChildNodes(textStyle)}
           </Text>
         )
       } else if (node.tagName === "BR") {
@@ -39,18 +41,14 @@ export const HtmlText: React.FC<Props> = ({ text, style }) => {
       } else if (node.tagName === "A") {
         return (
           <Text onPress={() => handleLink(node.attributes)}>
-            {node.childNodes.map((n, i) =>
-              renderViews(n, i, tw`text-blue-500`)
-            )}
+            {renderChildNodes(tw`text-blue-500`)}
           </Text>
         )
       } else if (node.tagName === "SPAN") {
         if ((node.attributes["class"] ?? "").includes("invisible")) {
           return null
         } else {
-          return (
-            <>{node.childNodes.map((n, i) => renderViews(n, i, textStyle))}</>
-          )
+          return <>{renderChildNodes(textStyle)}</>
         }
       } else {
         console.error(`unknown tag: ${node.tagName}`)
