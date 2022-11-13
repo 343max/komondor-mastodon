3896496b937f78c52b2e26d988b5eb2b0237444a
Max von Webel <max@343max.de>
2022-11-13 21:13:58 +0100

    better app.config.js formatting

--- app.config.js
+++ app.config.js
@@ -1,45 +1,45 @@
 export default {
-  "expo": {
-    "name": "Komondor",
-    "slug": "komondor-mastodon",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "komondor",
-    "userInterfaceStyle": "automatic",
-    "splash": {
-      "image": "./assets/images/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "Komondor",
+    slug: "komondor-mastodon",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "komondor",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": ["**/*"],
-    "ios": {
-      "buildNumber": "3",
-      "supportsTablet": true,
-      "bundleIdentifier": "app.komondor.mastodon",
-      "config": {
-        "usesNonExemptEncryption": false
-      }
-    },
-    "android": {
-      "versionCode": 2,
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      buildNumber: "3",
+      supportsTablet: true,
+      bundleIdentifier: "app.komondor.mastodon",
+      config: {
+        usesNonExemptEncryption: false,
       },
-      "package": "app.komondor.mastodon",
-      "blockedPermissions": ["android.permission.RECORD_AUDIO"]
     },
-    "web": {
-      "favicon": "./assets/images/favicon.png"
+    android: {
+      versionCode: 2,
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "app.komondor.mastodon",
+      blockedPermissions: ["android.permission.RECORD_AUDIO"],
     },
-    "extra": {
-      "eas": {
-        "projectId": "55f72a26-e82e-49a0-9e7b-68d5efd0bc2d"
-      }
-    }
-  }
+    web: {
+      favicon: "./assets/images/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "55f72a26-e82e-49a0-9e7b-68d5efd0bc2d",
+      },
+    },
+  },
 }
