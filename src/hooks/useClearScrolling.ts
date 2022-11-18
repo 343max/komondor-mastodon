2318feb2950df7196a9de300e799f61344c26dd2
Max von Webel <max@343max.de>
2022-11-18 15:03:31 +0100

    on all scroll events

--- src/hooks/useClearScrolling.ts
+++ src/hooks/useClearScrolling.ts
@@ -2,31 +2,20 @@ import React from "react"
 import { ScrollViewProps } from "react-native"
 
 export const useClearScrolling = (): [ScrollViewProps, boolean] => {
-  const [userIsScrolling, setUserIsScrolling] = React.useState(false)
   const [lastY, setLastY] = React.useState(0)
   const [scrolledDown, setScrolledDown] = React.useState(false)
 
-  const props: ScrollViewProps = React.useMemo(
-    () => ({
-      onScrollBeginDrag: ({ nativeEvent }) => {
-        setUserIsScrolling(true)
-        setLastY(nativeEvent.contentOffset.y)
-      },
-      onScrollEndDrag: () => {
-        setUserIsScrolling(false)
-      },
-      onScroll: ({ nativeEvent }) => {
-        if (userIsScrolling) {
-          setScrolledDown(
-            nativeEvent.contentOffset.y > lastY &&
-              nativeEvent.contentOffset.y > 50
-          )
-          setLastY(nativeEvent.contentOffset.y)
-        }
-      },
-    }),
-    [userIsScrolling]
-  )
+  const props: ScrollViewProps = {
+    onScrollBeginDrag: ({ nativeEvent }) => {
+      setLastY(nativeEvent.contentOffset.y)
+    },
+    onScroll: ({ nativeEvent }) => {
+      setScrolledDown(
+        nativeEvent.contentOffset.y > lastY && nativeEvent.contentOffset.y > 50
+      )
+      setLastY(nativeEvent.contentOffset.y)
+    },
+  }
 
   return [props, scrolledDown] as [ScrollViewProps, boolean]
 }
