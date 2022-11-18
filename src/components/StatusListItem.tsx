2a6cf607fc4722e1d0cb2e2a208b304c0622452f
Max von Webel <max@343max.de>
2022-11-18 10:48:39 +0100

    memo'ing StatusListItems

--- src/components/StatusListItem.tsx
+++ src/components/StatusListItem.tsx
@@ -1,8 +1,9 @@
+import React from "react"
 import { tw } from "../lib/tw"
 import { StatusView } from "./StatusView"
 
 type Props = Omit<React.ComponentProps<typeof StatusView>, "style">
 
-export const StatusListItem: React.FC<Props> = (props) => {
+export const StatusListItem = React.memo<Props>((props) => {
   return <StatusView {...props} style={tw`w-full p-2`} />
-}
+})
